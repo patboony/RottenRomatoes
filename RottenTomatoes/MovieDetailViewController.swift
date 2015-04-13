eef50d239489825538edc5b6a51f335e401a1ede
patboony <pat@DN0a232186.SUNet>
2015-04-12 19:32:42 -0700

    first commit

---
+++ RottenTomatoes/MovieDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MovieDetailViewController.swift
+//  RottenTomatoes
+//
+//  Created by Pat Boonyarittipong on 4/12/15.
+//  Copyright (c) 2015 patboony. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RottenTomatoes/MovieViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieViewCell.swift
+//  RottenTomatoes
+//
+//  Created by Pat Boonyarittipong on 4/10/15.
+//  Copyright (c) 2015 patboony. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
